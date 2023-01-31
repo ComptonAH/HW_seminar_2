1ed15287c3da2df3ac18c8ab81964ec1f48dbc6b
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-01-31 23:38:22 +0300

    The first excercise is complete

---
+++ exercise_13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Enter three digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>100)
+{
+    if (n<999)
+    {
+        n = n/10;
+        n = n%10;
+        Console.WriteLine($"the second digit is {n}");
+    }
+    else
+    {
+    Console.WriteLine("You wrote not a 3-digit number, please try again");
+    }
+}
+else
+{
+    Console.WriteLine("You wrote not a 3-digit number, please try again");
+}
