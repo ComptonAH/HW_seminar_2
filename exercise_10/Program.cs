ede9fb361ed2b96f39d33b8ac0329dd1577a9bdd
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-02 00:45:35 +0300

    Exercise 10 had been changed

--- exercise_10/Program.cs
+++ exercise_10/Program.cs
@@ -8,20 +8,13 @@
 Console.WriteLine("Enter three digit number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if (n>100)
-{
-    if (n<999)
-    {
-        n /= 10;
-        n %= 10;
-        Console.WriteLine($"the second digit is {n}");
-    }
-    else
-    {
-    Console.WriteLine("You wrote not a 3-digit number, please try again");
-    }
+if (n>=100 && n<=999)
+{    
+    n /= 10;
+    n %= 10;
+    Console.WriteLine($"the second digit is {n}");
 }
 else
-{
+    {
     Console.WriteLine("You wrote not a 3-digit number, please try again");
-}
+    }
